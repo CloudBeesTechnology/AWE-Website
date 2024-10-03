5646c312c0e88c84e524562e9ee2ffbe53ebf52b
KarthikeyanPeriyathambi <karthikeyandeveloper0307@gmail.com>
2024-10-03 07:20:08 +0530

    3-10:7:19am

--- src/pages/home/HomeHeader.jsx
+++ src/pages/home/HomeHeader.jsx
@@ -9,7 +9,7 @@ export const HomeHeader = () => {
 <div className="screen-size  max-sm:mt-32">
     <div className="flex overflow-hidden  justify-start items-start md:ml-20 ">
           <div className="w-full flex items-center max-sm:gap-2 max-sm:flex-col ">
-            <div className="space-y-10 sm:absolute bottom-20 right-14">
+            <div className="space-y-10 absolute bottom-20 sm:right-14">
               <h1 className="text-white max-sm:center max-md:text-center max-sm:text-[20px] text-[30px] font-medium ">
               The Professional Contractor Of Choice.
               </h1>
