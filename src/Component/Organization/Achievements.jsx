8bd37608a45fc95421f357499f3aed03a0056d0c
hari1614 <hariharanofficial2812@gmail.com>
2024-10-16 09:24:14 +0530

    updated: Made little changes on top section.

--- src/Component/Organization/Achievements.jsx
+++ src/Component/Organization/Achievements.jsx
@@ -1,4 +1,4 @@
-
+import { useEffect } from "react";
 import { CommonBanner } from "./CommonBanner";
 import bg from "../../assets/machines/Organization.jpg";
 import certificate1 from "../../assets/achievements/certificate1.png";
@@ -22,11 +22,12 @@ import certificate19 from "../../assets/achievements/certificate19.jpg";
 import certificate20 from "../../assets/achievements/certificate20.jpg";
 import certificate18 from "../../assets/achievements/certificate18.jpg";
 
+
 const certificates = [
-  certificate1,
+//   certificate1,
   certificate2,
-  certificate3,
   certificate13,
+  certificate3,
   certificate4,
   certificate5,
   certificate6,
@@ -40,7 +41,6 @@ const certificates = [
   certificate14,
   certificate15,
   certificate16,
-
   certificate19,
   certificate20,
   certificate18,
@@ -56,19 +56,24 @@ const certificateDescriptions = [
     "IAPS April Maintenance Campaign",
     "Achievement of 5 Years Free of Lost Time Injury (LTI)",
     "UBD Appreciation Award",
-    "MKJB Career Exhibition (2013)", // New
-  
-    "IBTE School of Energy and Engineering Satellite Market Day 2018", // New
-    "IBTE Sultan Bolkiah Campus Market Day 2019", // New
- 
-    "IBTE Central Market Day", // New
- 
-    "Pesta Kerjaya 2022", // New
-    "Recruitment Fair 2022", // New
-    "Pesta Convo IBTE 2023", // New
+    "MKJB Career Exhibition (2013)", 
+    "IBTE School of Energy and Engineering Satellite Market Day 2018", 
+    "IBTE Sultan Bolkiah Campus Market Day 2019", 
+    "IBTE Central Market Day", 
+    "Pesta Kerjaya 2022", 
+    "Recruitment Fair 2022", 
+    "Pesta Convo IBTE 2023", 
   ];
 
 const Achievements = () => {
+    
+    useEffect(() => {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }, []);
+
   return (
     <section>
       {/* Banner Section */}
@@ -79,8 +84,9 @@ const Achievements = () => {
         {/* <h2 className="text-center text-3xl font-bold mb-8">Our Achievements</h2> */}
 
         {/* First row: 4 certificates */}
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-12 mb-8">
-          {certificates.slice(0, 4).map((certificate, index) => (
+        <h2 className="text-xl font-bold py-4">ISO Certicates</h2>
+        <div className="grid grid-cols-2 lg:grid-cols-3 md:grid-cols-2 gap-12 mb-8">
+          {certificates.slice(0, 3).map((certificate, index) => (
             <div
               key={index}
               className="shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300"
@@ -88,13 +94,14 @@ const Achievements = () => {
               <img
                 src={certificate}
                 alt={`Certificate ${index + 1}`}
-                className="w-full h-auto object-cover"
+                className="w-full h-[200px] sm:h-[250px] md:h-[300px] object-fit"
               />
             </div>
-          ))}
+          ))} 
         </div>
 
         {/* Remaining rows: Polaroid style for images after the 4th */}
+        <h2 className="text-xl font-bold py-4">Awards</h2>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-6">
           {certificates.slice(4).map((certificate, index) => (
             <div
@@ -107,7 +114,7 @@ const Achievements = () => {
                 className="w-full h-[200px] sm:h-[250px] md:h-[300px] object-fit"
               />
               <div className="text-center mt-4">
-                <p>{certificateDescriptions[index]}</p> {/* Customize text if necessary */}
+                <p>{certificateDescriptions[index]}</p>
               </div>
             </div>
           ))}
