f7eb4e1ff428655014324afea9a7e3e23d4e14ea
hari1614 <hariharanofficial2812@gmail.com>
2024-10-16 09:55:52 +0530

    Modified: Made little change on text.

--- src/pages/about/AboutService.jsx
+++ src/pages/about/AboutService.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { CommonBanner } from "../../Component/Organization/CommonBanner";
 import bg from "../../assets/about/aboutUS.jpg";
-// import image1 from "../../assets/about/image4.png";
-// import image2 from "../../assets/about/image5.png";
+import { CommonBanner } from "../../Component/Organization/CommonBanner";
 import { HomeMS } from "../home/HomeMS";
 import { useEffect } from "react";
 
@@ -27,27 +25,11 @@ export const AboutService = () => {
           <div className=" flex-col max-sm:text-justify sm:mb-12 mb-10">
             <h2 className="text-center text-[28px] font-semibold bg-[#FEF116] w-[250px] p-3">What we do...</h2>
             <p className=" text-[20px] mt-10">
-              Adinin Works & Engineering Sdn. Bhd. (AWE) has extended its operation
+              Adinin Works & Engineering Sdn Bhd (AWE) has extended its operation
               and expertise into EPC Project execution, inclusive of offshore
               platforms and onshore plant construction.
             </p>
           </div>
-
-        
-
-          {/* Image Section */}
-          {/* <div className=" flex justify-center gap-4 max-w-6xl mx-auto pt-5">
-        <img
-          src={image1}
-          alt="Image 1"
-          className="w-1/2  rounded-lg shadow-md"
-        />
-        <img
-          src={image2}
-          alt="Image 2"
-          className="w-1/2  rounded-lg shadow-md"
-        />
-      </div> */}
         </div>
       </section>
       <HomeMS />
