b9066c82de4d44416bcc490eed46e7b19b8cd487
hari1614 <hariharanofficial2812@gmail.com>
2024-10-18 17:06:23 +0530

    Modified: Made some changes.

--- src/pages/about/AboutOurMission.jsx
+++ src/pages/about/AboutOurMission.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { CommonBanner } from '../../Component/Organization/CommonBanner';
+import React, { useEffect } from "react";
+import { CommonBanner } from "../../Component/Organization/CommonBanner";
 import bg from "../../assets/about/missionVision.jpg";
 
 export const AboutOurMission = () => {
@@ -17,7 +17,6 @@ export const AboutOurMission = () => {
 
       {/* Main Content */}
       <div className="w-full mt-12 px-8 lg:px-32">
-
         {/* Mission Section */}
         <h2 className="text-center py-4 px-2 text-lg lg:text-2xl bg-[#fef116] w-[190px]  font-semibold mb-4">
           Our Mission...
@@ -28,23 +27,28 @@ export const AboutOurMission = () => {
               num: 1,
               text: "To proactively engage our customers to seek better ways to serve their business needs.",
               bgColor: "bg-[#15B392]",
-              textColor: "text-white"
+              textColor: "text-white",
             },
             {
               num: 2,
               text: "To create an environment for employees to realize their full potential.",
               bgColor: "bg-[#FF8400]",
-              textColor: "text-white"
+              textColor: "text-white",
             },
             {
               num: 3,
               text: "To build long term relationships with business partners, suppliers, and financiers.",
               bgColor: "bg-[#379237]",
-              textColor: "text-white"
-            }
+              textColor: "text-white",
+            },
           ].map((mission, index) => (
-            <div key={index} className="flex items-center border py-6 px-8 rounded-lg">
-              <p className={`p-4 rounded-full h-10 w-10 flex items-center justify-center ${mission.bgColor} ${mission.textColor}`}>
+            <div
+              key={index}
+              className="flex items-center border py-6 px-8 rounded-lg"
+            >
+              <p
+                className={`p-4 rounded-full h-10 w-10 flex items-center justify-center ${mission.bgColor} ${mission.textColor}`}
+              >
                 {mission.num}
               </p>
               <p className="text-sm lg:text-base text-[#2a4255] leading-relaxed ml-4">
@@ -65,27 +69,32 @@ export const AboutOurMission = () => {
               num: 1,
               text: "We deliver consistent services and excellent solutions.",
               bgColor: "bg-[#836FFF]",
-              textColor: "text-white"
+              textColor: "text-white",
             },
             {
               title: "TO OUR EMPLOYEES",
               num: 2,
               text: "We are an environment where employees feel secure to release their aspirations.",
               bgColor: "bg-[#15133C]",
-              textColor: "text-white"
+              textColor: "text-white",
             },
             {
               title: "TO OUR INVESTORS",
               num: 3,
               text: "We achieve attractive shareholder returns.",
               bgColor: "bg-[#16423C]",
-              textColor: "text-white"
-            }
+              textColor: "text-white",
+            },
           ].map((vision, index) => (
-            <div key={index} className="flex flex-col items-center border py-6 px-8 rounded-lg">
+            <div
+              key={index}
+              className="flex flex-col items-center border py-6 px-8 rounded-lg"
+            >
               <h2 className="text-center font-bold mb-2">{vision.title}</h2>
               <div className="flex justify-center">
-                <p className={`p-4 rounded-full h-10 w-10 flex items-center justify-center ${vision.bgColor} ${vision.textColor}`}>
+                <p
+                  className={`p-4 rounded-full h-10 w-10 flex items-center justify-center ${vision.bgColor} ${vision.textColor}`}
+                >
                   {vision.num}
                 </p>
                 <p className="text-sm lg:text-base text-[#2a4255] leading-relaxed ml-4">
@@ -104,28 +113,32 @@ export const AboutOurMission = () => {
               num: 4,
               text: "We are a growing, well-managed, and diversified business entity.",
               bgColor: "bg-[#FFC700]",
-              textColor: "text-white"
+              textColor: "text-white",
             },
             {
               title: "TO OUR SUPPLIERS",
               num: 5,
               text: "We honor our commitment and we are a vehicle for their long-term growth.",
               bgColor: "bg-[#9BEC00]",
-              textColor: "text-white"
+              textColor: "text-white",
             },
-       
           ].map((vision, index) => (
-            <div key={index} className="flex flex-col items-center border py-6 px-8 rounded-lg">
+            <div
+              key={index}
+              className="flex flex-col items-center border py-6 px-8 rounded-lg"
+            >
               <h2 className="text-center font-bold mb-2">{vision.title}</h2>
               <div className="flex justify-center">
-                <p className={`p-4 rounded-full h-10 w-10 flex items-center justify-center ${vision.bgColor} ${vision.textColor}`}>
+                <p
+                  className={`p-4 rounded-full h-10 w-10 flex items-center justify-center ${vision.bgColor} ${vision.textColor}`}
+                >
                   {vision.num}
                 </p>
                 <p className="text-sm lg:text-base text-[#2a4255] leading-relaxed ml-4">
                   {vision.text}
                 </p>
               </div>
-            </div>    
+            </div>
           ))}
         </div>
         <div className="mt-8 grid grid-cols-1 lg:grid-cols-3 gap-8">
@@ -135,16 +148,23 @@ export const AboutOurMission = () => {
               num: 6,
               text: "We are trustworthy to our partners to achieve their business goals.",
               bgColor: "bg-[#CF4DCE]",
-              textColor: "text-white"
-            }
+              textColor: "text-white",
+            },
           ].map((vision, index) => (
-            <div key={index} className="flex flex-col items-center border py-6 px-8 rounded-lg">
+            <div
+              key={index}
+              className="flex flex-col items-center border py-6 px-8 rounded-lg"
+            >
               <div>
-              <h2 className="text-center font-bold mb-2 w-[200px]">{vision.title}</h2>
+                <h2 className="text-center font-bold mb-2 w-[200px]">
+                  {vision.title}
+                </h2>
               </div>
-              
+
               <div className="flex justify-center">
-                <p className={`p-4 rounded-full h-10 w-10 flex items-center justify-center ${vision.bgColor} ${vision.textColor}`}>
+                <p
+                  className={`p-4 rounded-full h-10 w-10 flex items-center justify-center ${vision.bgColor} ${vision.textColor}`}
+                >
                   {vision.num}
                 </p>
                 <p className="text-sm lg:text-base text-[#2a4255] leading-relaxed ml-4">
@@ -152,10 +172,8 @@ export const AboutOurMission = () => {
                 </p>
               </div>
             </div>
-            
           ))}
         </div>
-
       </div>
     </section>
   );
--- src/pages/home/HomeCelebration.jsx
+++ src/pages/home/HomeCelebration.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const HomeCelebration = () => {
   return (
-    <section className='screen-size w-full'>
-      <div className="center flex-col gap-4 max-sm:w-[280px]  max-sm:mb-10">
+    <section className='py-6 max-sm:py-0 screen-size w-full'>
+      <div className="center flex-col max-sm:gap-4 max-sm:w-[280px]  max-sm:mb-10">
         
           <div className='text-[#303030] max-sm:text-[12px] text-[20px] fond-medium mb-4"'>
           Thanks to each and every one of you for helping us achieve this important safety milestone.
